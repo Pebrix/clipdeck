37c0efd28aba23d2bd588b3946e33aeec622bcbf
Anupam <anupam.applications@gmail.com>
2026-02-02 00:31:36 +0530

    Initial Commit

---
+++ clipdeck/clipdeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipdeckApp.swift
+//  clipdeck
+//
+//  Created by Anupam Srivastava on 02/02/26.
+//
+
+import SwiftUI
+
+@main
+struct clipdeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
